b7e5663d63abaf05abde2f1ff378b0758f9d89a9
Otávio Lube dos Santos <otaviolube@gmail.com>
2022-10-19 08:30:58 -0300

    Criação da classe pessoa

---
+++ src/testegithub/Pessoa.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testegithub;
+
+/**
+ *
+ * @author alunolab11
+ */
+public class Pessoa {
+    
+    private String nome;
+    private String email;
+    private String cpf;
+
+    public Pessoa(String nome, String email, String cpf) {
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+    
+    
+}
